cae4eb5a43f9fa606b2100ff1876f1cf22569ad0
mino-94 <shmh2875@gmail.com>
2021-11-07 16:02:41 +0900

    smoker first commit

---
+++ src/main/java/com/smoker/cloud/run/Run.java
@@ -0,0 +1,16 @@
+package com.smoker.cloud.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Run {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Run.class, args);
+    }
+
+}
